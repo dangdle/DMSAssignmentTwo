215a50295c6bd5325b5aa020c9a944978fa3713a
Ashellan <Dangdle@hotmail.co.nz>
2015-05-08 17:22:27 +1200

    latest

--- src/nz/ac/aut/DMS/Hangouts/MainWindow.java
+++ src/nz/ac/aut/DMS/Hangouts/MainWindow.java
@@ -2,11 +2,14 @@ package nz.ac.aut.DMS.Hangouts;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 import nz.ac.aut.DMS.Hangouts.ServerStuff.Server;
+import nz.ac.aut.DMS.Hangouts.ServerStuff.ServerHangoutResponse;
 
-import java.util.List;
+import java.util.Date;
 
 /**
  * Created by Ashellan Edmonds on 7/05/15.
@@ -27,6 +30,15 @@ public class MainWindow extends Activity {
     }
 
     public void hangouts(View view) {
-        List<User> hangoutUsers = Server.getHangoutUsers();
+        Location location = MyLocationListener.lastKnownLocation;
+        if(location == null) {
+            Toast.makeText(this, "Location not found", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        ServerHangoutResponse hangout = Server.Hangout(username, String.valueOf(location.getLongitude()), String.valueOf(location.getLatitude()), new Date().toString());
+        if(hangout.equals(ServerHangoutResponse.SUCCESS)){
+
+        }
+
     }
 }
